6df6180b2d2e89ce73d6749960d7fb8e436eae26
Kaelubagu <caguiar@trinity.edu>
2023-09-03 02:18:22 -0500

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import {Poppins} from 'next/font/google'
 import {Silkscreen} from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 
 const silkscreen = Silkscreen({ 
@@ -22,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>{children}<Analytics /></body>
     </html>
   )
 }
