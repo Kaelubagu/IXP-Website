e17cbb4db599efc528d53589ab4960e93e7ea125
kaelubagu <caguiar@trinity.edu>
2023-09-02 20:01:20 -0500

    Iteration 1

---
+++ src/app/page.js
@@ -0,0 +1,104 @@
+import backgroundSize from 'tailwind.config.js'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillInstagram, AiFillFacebook} from 'react-icons/ai'
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <main class=" text-white">
+        {/* Navbar */}
+          <section class="flex flex-col min-h-screen" style={{backgroundImage: `url("/InRock.jpg")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundColor: 'rgba(0,0,0, 0.5)'
+    }}>
+            <nav class="sticky top-0 z-10 bg-black backdrop-filter backdrop-blur-md bg-opacity-30 border-b border-white">
+              <div class="max-w-5xl mx-auto px-4">
+                <div class="flex items-center justify-between h-16">
+                  <span class="text-2xl text-white hover:text-green-500 transition-colors, transition duration-500 ease-in-out font-semibold ">Iota Chi Rho</span>
+                  <div class="flex space-x-4 text-white">
+                    <a href="#">About</a>
+                    <a href="#">Active's Archive</a>
+                  </div>
+                </div>
+              </div>
+            </nav>
+
+        {/* Hero section */}  
+
+            <div class="flex-1 flex items-center ">
+              <div class="text-center mx-auto">
+                <h1 class="text-7xl font-semibold text-white hover:text-green-500 transition-colors, transition duration-500 ease-in-out">The Alternative Fraternity.</h1>
+              </div>
+            </div>
+
+          </section>
+        
+        {/* About */}
+        
+        <section class="p-5 bg-white">
+          <div class="text-center">
+            <h1 class="text-7xl text-center mt-20 mb-10 text-green-500 font-semibold">About Us</h1>
+            <img src="bros.jpg" width={600} height={600} class="rounded-lg mx-auto shadow-lg shadow-green-300"/>
+            <div class="container mx-auto">
+              <p class="text-gray-600 leading-9 p-10 text-md">
+              Iota Chi Rho is a social fraternity founded April 1992 at Trinity University in San Antonio, Texas. We are a unique take on fraternity life, attracting folks with off-beat interests and passions that drive us to lead and participate. Our members come from all majors, and actively participate in a variety of organizations. You can find us serving in leadership roles, producing summer research in the humanities and sciences, or participating in the performing arts on campus.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        {/*Active Members List*/}
+        <section class="bg-black container: mx-auto" >
+          <div class="text-center">
+            <h1 class="italic text-center mt-20 text-white text-2xl">Current Active Class:</h1>
+          </div>
+            <div class="text-center px-20">
+              <ul class="text-white text-md mt-4">
+                <li>Tucker Craft</li>
+                <li>Hues Gibson</li>
+                <li>Richard Grau</li>
+                <li>Max Hightower</li>
+                <li>Nathan Koehler</li>
+                <li>Elliot Schien</li>
+                <li>Andrew Phillips</li>
+                <li>Meyer Stellman</li>
+                <li>Jacob "JP" Palmer</li>
+              </ul>
+            </div>
+            <div class="text-center px-20">
+              <ul>
+              <li>Jake Borawski</li>
+              <li>Caleb Griffin</li>
+              <li>Henry Hafner</li>
+              <li>Joey Mattingly</li>
+              <li>Marcos Morales</li>
+              <li>Joshua O'Donnell-Paccione</li>
+              <li>Ashwin Ramesh</li>
+              <li>Echo Stellman</li>
+              <li>Preston Rhea</li>
+              <li>Julian Santovena</li>
+              </ul>
+            </div>
+          
+        </section>
+          
+        {/*Footer*/}
+        <section class="p-20">
+          <div class="flex justify-center gap-5 text-gray-600 text-4xl"> {/*social media images*/}
+            <AiFillInstagram/>
+            <AiFillFacebook/>
+          </div>
+        </section>
+        
+
+      </main>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        '50%': '50%',
+        '16': '4rem',
+      }
+    }
+  },
+  plugins: [],
+}
